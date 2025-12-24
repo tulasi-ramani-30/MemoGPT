6ea8d22efd35a964fdba8878bffaed80e2493451
tulasi-ramani-30 <tulasiramani08@gmail.com>
2025-12-24 15:29:33 +0530

    Add files via upload

---
+++ memogpt.py
@@ -0,0 +1,48 @@
+import json
+import os
+from google import genai
+import streamlit as st
+
+st.title("MemoGPT🤖")
+client = genai.Client(
+    api_key="AIzaSyDTgjNIUc3JoL6Vnglb3gcSz05ZnpkcLoA"
+)
+path = "history.json"
+
+if os.path.exists(path):
+    with open(path, "r") as file:
+        history = json.load(file)
+else:
+    history = []
+
+def answer_query(question, history):
+    conversation = ""
+    for entry in history:
+        conversation += f"User: {entry['question']}\n"
+        conversation += f"AI: {entry['answer']}\n"
+
+    conversation += f"User: {question}\nAI:"
+
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=conversation
+    )
+
+    return response.text
+
+user_input = st.text_area("Ask MemoGPT: ")
+ai_response = answer_query(user_input, history)
+if st.button("Enter"):
+    with st.spinner("Processing... ⏳"):
+        st.write(f"\nAI: {ai_response}")
+    
+
+history.append({
+    "question": user_input,
+    "answer": ai_response
+})
+
+with open(path, "w") as file:
+    json.dump(history, file, indent=4)
+
+
